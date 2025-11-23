8de78f13e02bdb4af4b9002e4c80a98d4252b9cd
kakerusaman <kakerusaman@gmail.com>
2025-11-24 00:31:03 +0900

    tailwindcssが導入されていなかったため導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 import { Sortable } from './components/Sortable';
-import { SortableItem } from './components/SortableItem';
+import { SortableItem } from './components//SortableItem';
 import { DndContext, type DragEndEvent } from '@dnd-kit/core';
+import './index.css';
 
 export default function Page() {
   const [items, setItems] = useState(
@@ -16,48 +17,6 @@ export default function Page() {
       ],
     },
   );
-
-  const sampleProjectData =
-{
-  id: 'PJ1',
-  name: 'Project 1',
-  description: 'This is project 1',
-  image_url: '',
-  lists: [
-    {
-      id: 'L1',
-      title: 'List 1',
-      tickets: [
-        {id: 'T1', title: 'Ticket 1', description: 'This is ticket 1'},
-        {id: 'T2', title: 'Ticket 2', description: 'This is ticket 2'},
-        {id: 'T3', title: 'Ticket 3', description: 'This is ticket 3'},
-      ],
-    },
-    {
-      id: 'L2',
-      title: 'List 2',
-      tickets: [
-        {id: 'T4', title: 'Ticket 4', description: 'This is ticket 4'},
-        {id: 'T5', title: 'Ticket 5', description: 'This is ticket 5'},
-        {id: 'T6', title: 'Ticket 6', description: 'This is ticket 6'},
-      ],
-    },
-    {
-      id: 'L3',
-      title: 'List 3',
-      tickets: [
-        {id: 'T7', title: 'Ticket 7', description: 'This is ticket 7'},
-        {id: 'T8', title: 'Ticket 8', description: 'This is ticket 8'},
-        {id: 'T9', title: 'Ticket 9', description: 'This is ticket 9'},
-      ],
-    },
-    {
-      id: 'L4',
-      title: 'List 4',
-      tickets: [],
-    },
-  ],
-}
 
   function handleDragEnd(event: DragEndEvent) {
     const {active, over} = event;
@@ -74,7 +33,7 @@ export default function Page() {
   }
   
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
+    <div className='bg-red text-gray-800 border-2 border-gray-300 rounded-lg p-4'>
       <DndContext
         onDragEnd={handleDragEnd}
         id={items.id}
